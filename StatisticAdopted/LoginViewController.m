3e981146bf63a6a4d63842d8b8e6110af42c2ba1
Costraci Mihail <mihaicostraci3@gmail.com>
2016-07-20 18:19:59 +0300

    ...

--- StatisticAdopted/LoginViewController.m
+++ StatisticAdopted/LoginViewController.m
@@ -36,6 +36,12 @@
     
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = NO;
+
+}
 
 - (IBAction)loginButtonTouched:(id)sender {
     
